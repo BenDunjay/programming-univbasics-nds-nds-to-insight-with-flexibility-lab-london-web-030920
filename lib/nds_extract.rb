c01ebfc22d1b731478401572a443fc4b6d982563
Ben Dunjay <bendunjay@outlook.com>
2020-02-19 20:58:28 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -94,7 +94,7 @@ def movies_with_directors_set(source)
  
  while director_index < source.length do 
    director_name = source[director_index]     # creating a variable holding full details of each source index
-   director_name[name] = director_name[:name] 
+   directors_movies.push(directors_name)
    
     director_index +=1 
  end
